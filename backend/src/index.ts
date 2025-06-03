863ce720ff77dc06d19156a0ef47a204c77afe06
s4094187 <s4094187@student.rmit.edu.au>
2025-06-04 09:50:11 +1000

    backend has been created, mysql2 instead of mysql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.routes";
 //import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 //app.use(cors());
 app.use(express.json());
