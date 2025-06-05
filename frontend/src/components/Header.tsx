3b6b7dce6a57ec8276ff55d85d7b3e89ae1a8674
s4094187 <s4094187@student.rmit.edu.au>
2025-06-05 14:38:40 +1000

    spread out navigation

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header" style={{width: "100%"}}>
       <Navigation />
     </div>
   );
