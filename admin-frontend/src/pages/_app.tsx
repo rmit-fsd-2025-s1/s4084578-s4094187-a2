be5644f37bf62f11c3f9083a16c2ad58981161bf
RyanFavaloro <s4084578@student.rmit.edu.au>
2025-06-07 22:02:09 +1000

    added password protection to admin app

--- admin-frontend/src/pages/_app.tsx
+++ admin-frontend/src/pages/_app.tsx
@@ -5,21 +5,40 @@ import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import AdminLogin from "@/components/AdminLogin";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+  const isLoginPage = router.pathname === "/login"
   
   return (
     <ApolloProvider client={client}>
       <ChakraProvider>
-        <div className = 'layout'>
-          <Header/>
-          <div className='content'>
-            <div className='main'>
-              <Component {...pageProps}/>
+        {isLoginPage ? (
+          <div className = 'layout'>
+              <Header/>
+              <div className='content'>
+                <div className='main'>
+                  <Component {...pageProps} />
+                </div>
+              </div>
+              <Footer/>
             </div>
-          </div>
-          <Footer/>
-        </div>
+        ) : (
+          <AdminLogin>
+            <div className = 'layout'>
+              <Header/>
+              <div className='content'>
+                <div className='main'>
+                  <Component {...pageProps} />
+                </div>
+              </div>
+              <Footer/>
+            </div>
+          </AdminLogin>
+        )}
       </ChakraProvider>
     </ApolloProvider>
   )
---
+++ admin-frontend/src/pages/login.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function AdminLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (username === "admin" && password === "admin") {
+      localStorage.setItem("isAdmin", "true");
+      router.push("/courses");
+    } else {
+      alert("Invalid admin credentials");
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h1>Admin Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
